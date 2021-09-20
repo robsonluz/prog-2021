eaac23b88f9d139bd9fa7c2a67dbcc5275216f4d
Robson João Padilha da Luz <robson.luz@gmail.com>
2021-09-20 13:08:01 +0000

    .

--- fibo.py
+++ fibo.py
@@ -1,3 +1,13 @@
-#Implemente um algoritmo que leia um número inteiro, em seguida calcule o fatorial deste número e apresente para o usuários.
-#Ex: n = 4
-#O Fatorial de 4 é 24
+# Implemente um algoritmo que imprima a sequência de fibonacci. Pergunte para o usuário quantos números da sequência de fibonacci ele quer que sejam impressos.
+# Ex: n = 6
+# 1, 1, 2, 3, 5, 8
+
+a = 0
+b = 1
+
+n = 10 
+for i in range (n):
+  print (b)
+  aux = b 
+  b = a + b
+  a = aux
