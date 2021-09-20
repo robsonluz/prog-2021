529747f1c66881796846bb3e281a6af6bf5b0ea8
BRUNA SCHWARTZ <88836751+brunaschwartz@users.noreply.github.com>
2021-09-20 13:05:56 +0000

    algoritmo fibonacci

---
+++ fibo.py
@@ -0,0 +1,13 @@
+# Implemente um algoritmo que imprima a sequência de fibonacci. Pergunte para o usuário quantos números da sequência de fibonacci ele quer que sejam impressos.
+# Ex: n = 6
+# 1, 1, 2, 3, 5, 8
+
+a = 0
+b = 1
+
+n = 10 
+for i in range (n):
+  print (b)
+  aux = b 
+  b = a + b
+  a = aux
