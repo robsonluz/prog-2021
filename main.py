36cfe139fa3926e8e0583d7230acf5105ff9604f
Robson João Padilha da Luz <robson.luz@gmail.com>
2021-09-20 12:45:35 +0000

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Implemente um algoritmo que imprima a sequência de fibonacci. Pergunte para o usuário quantos números, da sequência de fibonacci, ele quer que sejam impressos.
+# Implemente um algoritmo que imprima a sequência de fibonacci. Pergunte para o usuário quantos números da sequência de fibonacci ele quer que sejam impressos.
 # Ex: n = 6
 # 1, 1, 2, 3, 5, 8
 
