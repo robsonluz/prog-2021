0513aa24ba82f12c2debff1443ad7c2138b330d8
LORI NATSUMI <88836660+lorinatsumi@users.noreply.github.com>
2021-09-20 12:11:22 +0000

    finalização do algoritmo

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
-numeros = [10,20,5,30,1,3]
+numeros = [8,3,9,4,3,4,1,3,2]
 print(numeros)
-
-for i in range(5):
-  if numeros[i]>numeros[i+1]:
-    aux = numeros[i]
-    numeros[i]= numeros[i+1]
-    numeros[i+1]= aux
+for j in range(len(numeros)-1):
+  for i in range(len(numeros)-1):
+    if numeros[i]>numeros[i+1]:
+      aux = numeros[i]
+      numeros[i]= numeros[i+1]
+      numeros[i+1]= aux
 
 print (numeros)
 
