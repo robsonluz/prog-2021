5be229f6623073a6a66d61b09ad5c6b3ea1179bb
CAMILY MAYUMI <87656198+CamyMayumi@users.noreply.github.com>
2021-09-20 11:48:43 +0000

    primeiro passo da ordenação

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 
-
-print("ola")
-
-numeros = {10,20,5,30,1,3}
+numeros = [10,20,5,30,1,3]
 print(numeros)
 
+if numeros[0]>numeros[1]:
+  aux = numeros[0]
+  numeros[0]= numeros[1]
+  numeros[1]= aux
+
+if numeros [1]>numeros[2]:
+  aux = numeros[1]
+  numeros[1]= numeros[2]
+  numeros[2]= aux
+
+if numeros [2]>numeros[3]:
+  aux = numeros[2]
+  numeros[2]= numeros[3]
+  numeros[3]= aux
+
+if numeros [3]>numeros[4]:
+  aux = numeros[3]
+  numeros[3]= numeros[4]
+  numeros[4]= aux
+
+if numeros [4]>numeros[5]:
+  aux = numeros[4]
+  numeros[4]= numeros[5]
+  numeros[5]= aux
+
+print (numeros)
 
