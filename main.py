4f9ef669c4f03476dde00dffa25f47271b5de42c
Robson João Padilha da Luz <robson.luz@gmail.com>
2021-09-20 12:12:49 +0000

    melhorias na identação

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
 numeros = [8,3,9,4,3,4,1,3,2]
 print(numeros)
-for j in range(len(numeros)-1):
-  for i in range(len(numeros)-1):
-    if numeros[i]>numeros[i+1]:
+for j in range(len(numeros) - 1):
+  for i in range(len(numeros) - 1):
+    if numeros[i]  > numeros[i + 1]:
       aux = numeros[i]
-      numeros[i]= numeros[i+1]
-      numeros[i+1]= aux
+      numeros[i]= numeros[i + 1]
+      numeros[i + 1]= aux
 
 print (numeros)
 
